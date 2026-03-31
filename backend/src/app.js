e6441df7a44327753a65cbbde37003fbd66a8e38
mishra-khushboo <khush27093@gmail.com>
2026-03-31 16:30:29 +0530

    basic setup

---
+++ backend/src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { createServer } from "node:http";
+
+import { Server } from "socket.io";
+import { mongoose } from "mongoose";
+
+import cors from "cors";
+
+const app = express();
+
+app.get("/home", (req, res) => {
+	return res.json({"hello":"world"})
+})
+
+const start = async () => {
+	app.listen(8000, () => {
+		console.log("listing on port 8000")
+	})
+ }
+
+start();
