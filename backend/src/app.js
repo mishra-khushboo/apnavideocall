a0b7d05445c57b7e8c564fad34a18056340e9b53
mishra-khushboo <khush27093@gmail.com>
2026-04-04 18:45:01 +0530

    Connected MongoDB using mongoose

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,19 +2,30 @@ import express from "express";
 import { createServer } from "node:http";
 
 import { Server } from "socket.io";
-import { mongoose } from "mongoose";
-
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
 
+app.set("port", (process.env.PORT || 8000));
+app.use(cors());
+app.use(express.json({ limit: "40kb" }))
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+	
 app.get("/home", (req, res) => {
 	return res.json({"hello":"world"})
 })
 
 const start = async () => {
-	app.listen(8000, () => {
-		console.log("listing on port 8000")
+	const connectionDb = await mongoose.connect(process.env.MONGO_URI);
+	console.log(`Mongo connected db host : ${connectionDb.connection.host}`)
+	server.listen(app.get("port"), () => {
+		console.log("listening")
 	})
  }
 
