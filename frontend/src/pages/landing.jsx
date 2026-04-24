c28fb93a071137ddf98383001f6bf5d3594d8c3c
mishra-khushboo <khush27093@gmail.com>
2026-04-24 14:26:49 +0530

    design landing page

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import "../App.css";
 
 export default function LandingPage() {
@@ -20,6 +21,9 @@ export default function LandingPage() {
 				<div>
 					<h1><span style={{ color: "#FF9839" }}>Connect</span> with your loved Ones</h1>
 					<p>Cover a distance by Apna Video Call</p>
+					<div role='button'>
+						<Link to={"/home"}>Get Started</Link>
+					</div>
 				</div>
 				<div>
 					<img src="/mobile.png" alt=""></img>
